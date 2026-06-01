8cbb0b16e6ae88fe5f40ddb2d2381cd9e99e6e4b
Jet <jet@extremist.software>
2026-06-01 22:10:50 +0000

    primitive: update 1.0.1 bottle.

--- Formula/primitive.rb
+++ Formula/primitive.rb
@@ -13,12 +13,12 @@ class Primitive < Formula
   end
 
   bottle do
-    root_url "https://github.com/primitivedotdev/homebrew-tap/releases/download/primitive-1.0.0"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "e2a7f320a76beab5404d7626875ad938f57583aa4dec60039f568bf0fb31cb56"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "9702a06f2e4f7a036868591ba85b26b88441b179e7f7685278e9ae6555fab9c6"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "eb8209421d9819617543a6b29802a5080f3cd3e90d527abee68402f5661fb4a3"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "0745ba4bcd6417f718407d1eba4cbe892522ea570555d92ee32dd173250beae5"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "4deb5a5e8d05f0a477bba8d446339281b1d6478bcb3c2d5bb252034184fc87d8"
+    root_url "https://github.com/primitivedotdev/homebrew-tap/releases/download/primitive-1.0.1"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "f431e31735170946f1d47db8c60ad79e5cef48ae305654b28229c59b1e958c2a"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "e615f93e2ac4ffedbbf88f3812e2379522a90b38c26f9c681b3d55e9af186920"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "615de98ae57c3ee0afd4b46bd9d27c91ef9cf165306d9c5891d6808900176e91"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "0b553dec9a5b7f5fcf97e84cee7309bd01ec72adda504e8e6c76fa321d4323aa"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e80c7faab73c3b0a7ce6e5e8546a52e156eb99bf3385617dbf32b98a7da89142"
   end
 
   depends_on "node"
